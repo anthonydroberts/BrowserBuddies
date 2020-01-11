9cd305d7b5ae6895470a52d029ee5ec35116c18d
Tony Roberts <123ananan@gmail.com>
2020-01-11 17:57:18 -0500

    initial DB ops for links

--- data/app.js
+++ data/app.js
@@ -1,49 +1,68 @@
 //imports
 const visitorManager = require(`./visitors.js`);
-const cors = require('cors')
-const { db } = require(`./initDB.js`)
+const linkManager = require(`./links.js`);
+const cors = require('cors');
+const { db } = require(`./initDB.js`);
+const bodyParser = require(`body-parser`);
 
-// init express and others
-const express = require('express')
-const app = express()
-const port = 3000
-const IP = "0.0.0.0"
+// init express and others plus configure
+const express = require('express');
+const app = express();
+const port = 3000;
+const IP = "0.0.0.0";
 
 // server vars
 let hitTracker = 0;
 
 // allow x origin 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // protocols
 app.get('/', (req, res) => {
     
     hitTracker = hitTracker + 1;
 
-    console.log(`${req.connection.remoteAddress} requested '/'`);
+    console.log(`${req.connection.remoteAddress} requested (GET) '/'`);
     visitorManager.trackVisit(req.connection.remoteAddress);
     res.send(`
         <p>server root</p>
         </br>
         <p>Total visits: ${hitTracker}</p>
     `);
-})
+});
 
-app.get('/links', (req, res) => {
-    let data = 
-        {links:[
-            {title:'t1'},
-            {title:'t2'},
-            {title:'t3'},
-            {title:'t4'},
-            {title:'t5'},
-            {title:'t6'}
-        ]};
+app.get('/links', async (req, res) => {
+    console.log(`${req.connection.remoteAddress} requested (GET) '/links'`);
+    
+    let result = await linkManager.getLinks();
+    
+    console.log(`Returned links: ${JSON.stringify(result)}`);
 
-    console.log(`${req.connection.remoteAddress} requested '/links'`);
     visitorManager.trackVisit(req.connection.remoteAddress);
-    res.send(data);
-})
+    res.send(result);
+});
+
+app.post('/links', (req, res) => {
+    console.log(`${req.connection.remoteAddress} requested (POST) '/links' with data: ${JSON.stringify(req.body)}`);
+
+    console.log(req.body.link.url);
+    console.log(req.body.link.title);
+
+    linkManager.addLink("ujCh08UDw9flKPhq142N", req.body.link.url, req.body.link.title);
+
+    visitorManager.trackVisit(req.connection.remoteAddress);
+    res.send(req.body);
+});
+
+app.post('/deleteLink', (req, res) => {
+    console.log(`${req.connection.remoteAddress} requested (POST) '/deleteLink'`);
+
+    linkManager.deleteLink(`ujCh08UDw9flKPhq142N`, req.body.url);
+
+    visitorManager.trackVisit(req.connection.remoteAddress);
+});
 
 app.listen(port, IP, (err) => {
     if (err) {
@@ -52,4 +71,4 @@ app.listen(port, IP, (err) => {
 
     console.log(`Starting server: ${IP}:${port}`)
     console.log(`Access: http://192.168.0.13:${port}/`)
-})
+});
