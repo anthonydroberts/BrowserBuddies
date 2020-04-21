5a6f8a0f5a5df6f6ba65a8b4ba5c75bc8b5eccbd
Maya Saringan <mayas.saringan@gmail.com>
2020-04-21 00:19:54 -0400

    Update frontend (#5)
    * frontend update
    * Detail readmes
    * Take out excess files

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,30 +1,45 @@
 /* global chrome */
 import React from 'react';
-import logo from './logo.svg';
+import logo from './browser-buddies-logo.svg';
 import './App.css';
-import LinesEllipsis from 'react-lines-ellipsis'
+import LinkItem from './LinkItem'
+
+const ServerLinks = {
+  get: 'http://desktop-qdjf5ba:3000/links',
+  send: 'http://desktop-qdjf5ba:3000/links',
+  delete: 'http://desktop-qdjf5ba:3000/deleteLink'
+}
+
+Object.freeze(ServerLinks);
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      links : []
+      links : [] //store currently stored tab links
     }
   }
-  getLinks = () =>{
-    return this.state.links
+
+  getLinks = () => this.state.links
+  
+  updateLinks = () => {
+    fetch( ServerLinks.get, { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+    .then (res=>res.json())
+    .then(l => {
+      console.log(l);
+      this.setState({links:l});
+    })
+    .catch(e=>{
+      console.log(e);
+    });
   }
 
   componentDidMount() {
-      try {
-      // setInterval( () => {
-          console.log("interval elapse")
-          fetch('http://192.168.0.13:3000/links',{ 
-          'Content-Type': 'application/json', 'Accept': 'application/json'}).then(res=>res.json()).then(l => {console.log(l);this.setState({links:l})})
-    //  }, 5000);
-      } catch(e) {
-        console.log(e);
-      }
+      this.updateLinks();
+      setInterval(this.updateLinks,60000); // query server for update on links every minute
   }
 
   sendCurrentTab = () =>{
@@ -33,53 +48,92 @@ export default class App extends React.Component {
         url: t[0].url,
         title: t[0].title
       }
-      fetch('http://192.168.0.13:3000/links',{
+      fetch(ServerLinks.send,{
         method:'POST',
         body:JSON.stringify({link:link}),
         headers: {
           'Content-Type': 'application/json'
         },
-      }).then(res=>res.json()).then(l => {console.log(l);})
+      }).then(res=>{
+        setTimeout(this.updateLinks, 500)
+        return res.json();
+      }
+      ).then(l => {
+        console.log(l);
+      })
     });
   }
 
   deleteTab = (link) =>{
-    fetch('http://192.168.0.13:3000/deleteLink',{
+    console.log(link)
+    fetch(ServerLinks.delete,{
       method:'POST',
       body:JSON.stringify({url:link.url}),
       headers: {
         'Content-Type': 'application/json'
       }
-    }).then(res=>res.json()).then(l => {console.log(l);})
+    })
+    .then( res => {
+      setTimeout(this.updateLinks, 500);
+      return res.json();
+    })
+    .then( l => {
+      console.log(l);
+      this.updateLinks();
+    })
+    .catch( e => console.log(e));
   };
 
-  renderLink = (item) =>{
-    return (
-      <div style={{padding:3,flexDirection:'row',display:'flex', justifyContent:'space-between'}}>
-        <a className='link' href={item.url} style={{flexDirection:'row',display:'flex',justifyContent:'flex-start', maxWidth:'80%' }}>
-          <LinesEllipsis text={item.title} maxLine={1} basedOn='letters' ellipsis='...' trimRight style={{color:"white",fontWeight:'bold', marginRight:10}}/>
-          <LinesEllipsis text={item.url} maxLine={1} basedOn='letters' ellipsis='...' trimRight style={{fontStyle:'italic', color:"#9c95b7",}}/>
-        </a>
-        <button onClick={() => this.deleteTab(item)} style={{fontSize:10, display:'flex',flexDirection:'row',height:20,fontWeight:'bold', backgroundColor:"#221f2e", color:'white', borderWidth:0}}>REMOVE</button>
-      </div>
-    );
+  getFavicon =  (link , linkIdx) => {
+    // Google API doesn't always fetch github favicon. Handle github links manually instead
+    if (link.includes("github.com")) {
+      let links = this.state.links.splice(0);
+      links[linkIdx] = {...links[linkIdx], faviconUrl: "https://github.com/apple-touch-icon.png"};
+      this.setState({links:links});
+      return;
+    }
+
+    fetch("https://cors-anywhere.herokuapp.com/https://api.statvoo.com/favicon/?url="+link,{ method: 'GET', })
+    .then(res=>{
+      if (res.ok)
+      {
+        let links = this.state.links.splice(0);
+        links[linkIdx] = {...links[linkIdx], faviconUrl: "https://cors-anywhere.herokuapp.com/https://api.statvoo.com/favicon/?url="+link}
+        this.setState({links:links})
+      }
+      return true; 
+    })
+    .catch((e)=>{
+      console.log(e);
+    })
+    
   }
-  render = ()=>{
-  return  (
-    <div className="Container" style={{width:500}}>
-      <div className="App" style={{padding:10, display:'flex',flexDirection:'column'}}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" width={50} height={50} />
-          <p>Browser Buddies</p>
-        </header>
-        <div className="Links-display" style={{flexDirection:'column',padding:10,flex:1, justifyContent:'space-between'}}>
-          <div style={{marginBottom:10}}>
-            {this.getLinks().map(item => this.renderLink(item))}
+  render =  () => {
+    return  (
+      <div className="Container" >
+        <div className="App">
+          <header className="App-header">
+            <div className="app-header-subtext-container">
+            <div className="app-header-subtext">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Browser Buddies</h1>
+          
+            </div>
+            </div>
+            {/*<p>Logged in as Anony Mouse</p>*/}
+          </header>
+          <div className="Links-display" style={{flexDirection:'column',flex:1, justifyContent:'space-between'}}>
+            <div className="group-buttons-container">
+              <button onClick={this.sendCurrentTab} className="group-button">MY LINKS</button>
+            </div>
+            <div className="group-links-list">
+              {this.getLinks().map((item, idx) => <LinkItem item={item} index={idx} requestFaviconFn={this.getFavicon} deleteTabFn={this.deleteTab}/>)}
+            </div>
+          
           </div>
-          <button onClick={this.sendCurrentTab} style={{height:40, padding:10,fontSize:15, fontWeight:'bold', backgroundColor:"#221f2e", color:'white', borderWidth:0}}>+ current tab {this.state.test}</button>
+          <button onClick={this.sendCurrentTab} >Add Tab</button>
         </div>
       </div>
-    </div>
-  );
-} 
+    );
+  } 
 }
